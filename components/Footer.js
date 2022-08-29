f915ac766762b4b2b473caab849ababddaa31787
akulb42 <112406240+akulb42@users.noreply.github.com>
2022-08-29 20:03:53 +0530

    added view source link

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,8 @@ const Footer = () => {
   return (
     <div className="bottom-0 w-screen h-16 flex flex-row bg-gray-100 items-center justify-center mt-8">
       <h1 className=" tex-bold p-2">
-        &copy; 2022 Growthex. <a>View Source</a>
+        &copy; 2022 Growthex.{' '}
+        <a href="https://github.com/akulb42/growthex">View Source</a>
       </h1>
     </div>
   );
